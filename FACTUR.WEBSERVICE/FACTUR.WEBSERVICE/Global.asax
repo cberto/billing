d2c9eaf75004c2906f9374b145336c0015bd5c23
cberto <bertolanicami@gmail.com>
2022-01-28 00:00:49 -0300

    first commit

---
+++ FACTUR.WEBSERVICE/FACTUR.WEBSERVICE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FACTUR.WEBSERVICE.Global" Language="C#" %>
---
+++ facturationSystem.web/facturationSystem/Controllers/CompanyRegistrationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace facturationSystem.Controllers
+{
+    public class CompanyRegistrationController : Controller
+    {
+      
+        public ActionResult CompanyRegistration()
+        {
+            return View();
+        }
+
+        
+    }
+}
